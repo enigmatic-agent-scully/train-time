de9b3878e567f10f1b2bc26f65c6638bda3f4419
enigmatic-agent-scully <amanda.elisabet.jordan@gmail.com>
2018-09-30 15:12:25 -0400

    minor change

--- assets/javascript/choochoo.js
+++ assets/javascript/choochoo.js
@@ -12,7 +12,6 @@ $(document).ready(function () {
     firebase.initializeApp(config);
 
     var database = firebase.database();
-    var now = moment().format("HH:mm");
     var train = "";
     var dest = "";
     var start = "";
@@ -22,25 +21,15 @@ $(document).ready(function () {
     $("#go").on("click", function (event) {
         event.preventDefault();
         train = $("#train").val().trim();
-        console.log(train);
         dest = $("#destination").val().trim();
-        console.log(dest);
         start = $("#start").val().trim();
-        console.log(start);
-        time = moment(start);
-        console.log(time);
         freq = $("#frequency").val();
-        console.log(freq);
-        now = moment().format("HH:mm");
-        console.log(now);
-
 
         var trainData = {
             name: train,
             destination: dest,
             startTime: start,
             frequency: freq,
-            nowtime: now,
         };
         console.log(trainData);
         database.ref().push(trainData);
@@ -50,15 +39,13 @@ $(document).ready(function () {
     });
 
     database.ref().on("child_added", function (childSnapshot) {
-        now = moment().format("HH:mm");
         console.log(childSnapshot.val());
         console.log(childSnapshot.val().name);
         console.log(childSnapshot.val().destination);
         console.log(childSnapshot.val().startTime);
         console.log(childSnapshot.val().frequency);
-        console.log(childSnapshot.val().nowtime);
         var object = childSnapshot.val();
-        var firstTime = moment(object.startTime, "HH:mm").format();
+        var firstTime = moment(object.startTime, "hh:mm a").format();
         console.log(firstTime);
         var diffTime = moment().diff(moment(firstTime), "minutes");
         console.log(diffTime);
@@ -67,7 +54,7 @@ $(document).ready(function () {
         var minTillTrain = object.frequency - tRemainder;
         console.log(minTillTrain);
         var nextTrain = moment().add(minTillTrain, "minutes");
-        nextTrain = moment(nextTrain).format("HH:mm");
+        nextTrain = moment(nextTrain).format("hh:mm a");
         $("tbody").append(`<tr>
                 <td id="train">${childSnapshot.val().name}</td>
                 <td id="destination">${childSnapshot.val().destination}</td>
