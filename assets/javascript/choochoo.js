53a8b546b37794dd44609339c87de40e6caba5fd
AJ <amanda.elisabet.jordan@gmail.com>
2018-12-20 15:03:31 -0500

    changed freq form field input type to number - not quite validated but close-ish

--- assets/javascript/choochoo.js
+++ assets/javascript/choochoo.js
@@ -1,88 +1,75 @@
+$(document).ready(function() {
+  var config = {
+    apiKey: 'AIzaSyAMcV07MCzpWAe4zCJK9Es2h94CTRnKuc4',
+    authDomain: 'fir-project-1-d4265.firebaseapp.com',
+    databaseURL: 'https://fir-project-1-d4265.firebaseio.com',
+    projectId: 'fir-project-1-d4265',
+    storageBucket: 'fir-project-1-d4265.appspot.com',
+    messagingSenderId: '315625469189'
+  };
 
-// Still haven't solved the mystery problem!
+  firebase.initializeApp(config);
 
-$(document).ready(function () {
-    var config = {
-        apiKey: "AIzaSyAMcV07MCzpWAe4zCJK9Es2h94CTRnKuc4",
-        authDomain: "fir-project-1-d4265.firebaseapp.com",
-        databaseURL: "https://fir-project-1-d4265.firebaseio.com",
-        projectId: "fir-project-1-d4265",
-        storageBucket: "fir-project-1-d4265.appspot.com",
-        messagingSenderId: "315625469189"
-    };
+  var database = firebase.database();
 
-    firebase.initializeApp(config);
+  $('#go').on('click', function(event) {
+    event.preventDefault();
+    var freq = $('#frequency')
+      .val()
+      .trim();
+    if (parseInt(freq) === NaN) {
+      alert('Please enter a number!');
+    } else {
+      var train = $('#train')
+        .val()
+        .trim();
+      var dest = $('#destination')
+        .val()
+        .trim();
+      var start = $('#start')
+        .val()
+        .trim();
 
-    var database = firebase.database();
+      var now = moment().format('hh:mm a');
 
-    $("#go").on("click", function (event) {
-        event.preventDefault();
-        console.log($(this));
-        console.log(event);
-        var train = $("#train").val().trim();
-        console.log(train);
-        var dest = $("#destination").val().trim();
-        console.log(dest);
-        var start = $("#start").val().trim();
-        console.log(start);
-        var time = moment(start);
-        console.log(time);
-        var freq = $("#frequency").val().trim();
-        console.log(freq);
-        var now = moment().format("hh:mm a");
-        console.log(now);
+      var trainData = {
+        name: train,
+        destination: dest,
+        startTime: start,
+        frequency: freq,
+        nowtime: now
+      };
 
-        var trainData = {
-            name: train,
-            destination: dest,
-            startTime: start,
-            frequency: freq,
-            nowtime: now,
-        };
-        console.log(trainData);
+      database.ref().push(trainData);
+      trainData = {};
+      document.getElementById('form').reset();
+      return false;
+    }
+  });
 
-        database.ref().push(trainData);
-        trainData = {};
-        document.getElementById("form").reset();
-        return false;
-    });
+  database.ref().on(
+    'child_added',
+    function(childSnapshot) {
+      var train = childSnapshot.val().name;
+      var dest = childSnapshot.val().destination;
+      var freq = childSnapshot.val().frequency;
+      var object = childSnapshot.val();
+      var firstTime = moment(object.startTime, 'hh:mm a').format();
+      var diffTime = moment().diff(moment(firstTime), 'minutes');
+      var tRemainder = diffTime % object.frequency;
+      var minTillTrain = object.frequency - tRemainder;
+      var nextTrain = moment().add(minTillTrain, 'minutes');
+      nextTrain = moment(nextTrain).format('hh:mm a');
 
-    database.ref().on("child_added", function (childSnapshot) {
-        console.log(childSnapshot.val());
-        var train = childSnapshot.val().name;
-        console.log(train);
-        var dest = childSnapshot.val().destination;
-        console.log(dest);
-        var start = childSnapshot.val().startTime;
-        console.log(start);
-        var freq = childSnapshot.val().frequency;
-        console.log(freq);
-        var now = childSnapshot.val().nowtime;
-        console.log(now);
-
-        var object = childSnapshot.val();
-        var firstTime = moment(object.startTime, "hh:mm a").format();
-        console.log(firstTime);
-        var diffTime = moment().diff(moment(firstTime), "minutes");
-        console.log(diffTime);
-        var tRemainder = diffTime % object.frequency;
-        console.log(tRemainder);
-        var minTillTrain = object.frequency - tRemainder;
-        console.log(minTillTrain);
-        var nextTrain = moment().add(minTillTrain, "minutes");
-        nextTrain = moment(nextTrain).format("hh:mm a");
-
-        $("tbody").append(`<tr>
+      $('tbody').append(`<tr>
                 <td>${train}</td>
                 <td>${dest}</td>
                 <td>${freq}</td>
                 <td>${nextTrain}</td>
                 <td>${minTillTrain}</td></tr>`);
     },
-        function (errorObject) {
-            console.log("Errors handed:" + errorObject.code);
-        });
-
-
-}
-)
+    function(errorObject) {
+      console.log('Errors handed:' + errorObject.code);
+    }
+  );
+});
